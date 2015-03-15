4439e61a5992f8da3dbcf612c760b635de07d5d5
Taylor Frey <frey099@regis.edu>
2015-03-15 12:05:57 -0600

    Applied stashed changes from Main Development

---
+++ MSSE652-iOSProject/SCISProgramTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCISProgramTableViewController.h
+//  MSSE652-iOSProject
+//
+//  Created by Taylor Frey on 3/15/15.
+//  Copyright (c) 2015 Taylor Frey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCISProgramTableViewController : UITableViewController
+
+@end
